a6a750c41ecc4837bca4742fb3ce58fb7395f63d
junminhong <junminhong1110@gmail.com>
2021-05-07 13:42:47 +0800

    complete 14. Longest Common Prefix

---
+++ README.md
@@ -0,0 +1,3 @@
+|Date|Title|Runtime|Memory|Language|Link|Remark|
+|:-|:-|:-|:-|:-|:-|:-|
+|20210414|Two Sum|4ms|3.2MB|golang| [<img src="imgs/business.png" style="width:100%;">](https://github.com/junminhong/Everday_LeetCode/tree/master/Code/2020/04/18)  |可在微調演算法|
--- easy/14. Longest Common Prefix.go
+++ easy/14. Longest Common Prefix.go
@@ -2,24 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
-	strAry := []string{"flower", "flow", "flight"}
-	longestCommonPrefix(strAry)
-}
-
-func longestCommonPrefix(strAry []string) string {
-	prefix := ""
-	for _, value := range strAry {
-		if prefix == "" {
-			prefix = value
+func prefix(result *string, str string, size int) {
+	tmp := ""
+	for j := 0; j < size; j++ {
+		resultTmp := *result
+		if resultTmp[j] == str[j] {
+			tmp += string(str[j])
 		} else {
-			for _, prefixValue := range prefix {
-				for _, valueValue := range value {
-					fmt.Println(prefixValue)
-					fmt.Println(valueValue)
-				}
-			}
+			break
 		}
 	}
-	return ""
+	*result = tmp
+}
+
+func longestCommonPrefix(strs []string) string {
+	result := strs[0]
+	for i := 1; i < len(strs); i++ {
+		if len(result) > len(strs[i]) {
+			prefix(&result, strs[i], len(strs[i]))
+		} else {
+			prefix(&result, strs[i], len(result))
+		}
+	}
+	return result
+}
+func main() {
+	//strs := []string{"flower", "flow", "flight", "flow"}
+	strs := []string{"dog", "racecar", "car"}
+	fmt.Println(longestCommonPrefix(strs))
 }
